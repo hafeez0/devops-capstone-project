ce2c13c42443235d561f9bfb3f870e2b9fe69660
hafeez0 <71581184+hafeez0@users.noreply.github.com>
2025-07-25 15:23:57 +0300

    Create jenkins.groovy

---
+++ app/jenkins.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+
+    environment {
+        DOCKER_IMAGE = 'hafeez0/flask-devops-app'
+        DOCKERHUB_CREDENTIALS = 'dockerhub'
+    }
+
+    stages {
+        stage('Clone Repo') {
+            steps {
+                git branch: 'main', url: 'https://github.com/hafeez0/devops-capstone-project.git'
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    docker.build("${DOCKER_IMAGE}")
+                }
+            }
+        }
+
+        stage('Push to Docker Hub') {
+            steps {
+                script {
+                    docker.withRegistry('', "${DOCKERHUB_CREDENTIALS}") {
+                        docker.image("${DOCKER_IMAGE}").push('latest')
+                    }
+                }
+            }
+        }
+    }
+}
